619cfe07728711fa007979fde0644e5215204bb0
Mohammed Ismail <mohammedismail97420@gmail.com>
2023-05-31 15:06:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# React App
+
+## Getting Started
+
+1. Clone this repository and navigate to the project directory.
+2. Install the project dependencies with `npm install` or `yarn install`.
+3. Start the development server with `npm start` or `yarn start`.
+4. Open `http://localhost:3000` in your browser to view the app.
+
+## Available Scripts
+
+- `npm start` or `yarn start`: Starts the development server.
+- `npm test` or `yarn test`: Launches the test runner.
+- `npm run build` or `yarn build`: Builds the app for production.
---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+const App = () => {
+  interface IUser {
+    name: {
+      first: "";
+      last: "";
+    };
+    email: string;
+  }
+  const [user, setUser] = useState<IUser>({
+    name: { first: "", last: "" },
+    email: "",
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  //Function to fetch data
+  const fetchData = async () => {
+    setIsLoading(true);
+    await axios
+      .get("https://randomuser.me/api")
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data.results[0]));
+        setUser(response.data.results[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  //Call fetch function on page load
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>User info</h1>
+      <div className="flex">
+        <h4>Name: </h4>
+        <p>{user.name.first + " " + user.name.last}</p>
+      </div>
+      <div className="flex">
+        <h4>Email: </h4>
+        <p>{user.email}</p>
+      </div>
+
+      <button disabled={isLoading} className="btn" onClick={() => fetchData()}>
+        {isLoading ? "Loading..." : "Refresh"}
+      </button>
+    </div>
+  );
+};
+
+export default App;
